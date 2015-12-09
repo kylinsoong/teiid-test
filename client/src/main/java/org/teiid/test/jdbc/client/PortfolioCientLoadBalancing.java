b5e567951ee9e7e3071859c11a7e2a6f34fbbfb9
kylinsoong <kylinsoong.1214@gmail.com>
2015-12-09 18:17:18 +0800

    add client code

--- client/src/main/java/org/teiid/test/jdbc/client/PortfolioCientLoadBalancing.java
+++ client/src/main/java/org/teiid/test/jdbc/client/PortfolioCientLoadBalancing.java
@@ -1,15 +1,11 @@
 package org.teiid.test.jdbc.client;
 
-import static org.teiid.example.util.JDBCUtils.getDriverConnection;
-import static org.teiid.example.util.JDBCUtils.execute;
-
 import java.sql.Connection;
 
-
-public class PortfolioCient {
+public class PortfolioCientLoadBalancing {
 	
 	private static final String JDBC_DRIVER = "org.teiid.jdbc.TeiidDriver";
-    private static final String JDBC_URL = "jdbc:teiid:Portfolio@mm://localhost:31000;version=1";
+    private static final String JDBC_URL = "jdbc:teiid:Portfolio@mm://localhost:31000,localhost:31150;version=1";
     private static final String JDBC_USER = "teiidUser";
     private static final String JDBC_PASS = "password1!";
     
@@ -18,18 +14,16 @@ public class PortfolioCient {
     static String sql_PRODUCT = "SELECT * FROM PRODUCT";
     static String sql_Stock = "SELECT * FROM Stock";
     static String sql_PersonalHoldings = "SELECT * FROM PersonalHoldings";
-    
+
 	public static void main(String[] args) throws Exception {
 
-		Connection conn = getDriverConnection(JDBC_DRIVER, JDBC_URL, JDBC_USER, JDBC_PASS);
+		Connection conn = JDBCUtils.getDriverConnection(JDBC_DRIVER, JDBC_URL, JDBC_USER, JDBC_PASS);
 		
-		execute(conn, sql_sheet1, false);
-		execute(conn, sql_PersonalHoldings, false);
-		execute(conn, sql_StockPrices, false);
-		execute(conn, sql_PRODUCT, false);
-		execute(conn, sql_PRODUCT, false);
-		execute(conn, sql_Stock, true);
-		
+		JDBCUtils.execute(conn, sql_sheet1, false);
+		JDBCUtils.execute(conn, sql_PersonalHoldings, false);
+		JDBCUtils.execute(conn, sql_StockPrices, false);
+		JDBCUtils.execute(conn, sql_PRODUCT, false);
+		JDBCUtils.execute(conn, sql_Stock, true);
 	}
 
 }
