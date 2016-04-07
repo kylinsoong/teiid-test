f2b627bd37134eae41d53c70ec119598684b7934
kylinsoong <kylinsoong.1214@gmail.com>
2016-04-07 18:44:08 +0800

    add teiid 3952 test code

---
+++ perf/src/main/java/org/teiid/test/teiid3952/TEIID3952Reproduce.java
@@ -0,0 +1,86 @@
+package org.teiid.test.teiid3952;
+
+import static org.teiid.test.Constants.H2_JDBC_DRIVER;
+import static org.teiid.test.Constants.H2_JDBC_PASS;
+import static org.teiid.test.Constants.H2_JDBC_URL;
+import static org.teiid.test.Constants.H2_JDBC_USER;
+import static org.teiid.test.util.JDBCUtils.execute;
+
+import java.io.InputStreamReader;
+import java.sql.Connection;
+import java.util.Properties;
+import java.util.logging.Level;
+
+import javax.sql.DataSource;
+
+import org.h2.tools.RunScript;
+import org.teiid.runtime.EmbeddedConfiguration;
+import org.teiid.runtime.EmbeddedServer;
+import org.teiid.test.teiid3480.InternalMaterialization;
+import org.teiid.test.util.EmbeddedHelper;
+import org.teiid.translator.jdbc.h2.H2ExecutionFactory;
+
+public class TEIID3952Reproduce {
+
+    static EmbeddedServer server = null;
+    static Connection conn = null;
+    
+    public static void main(String[] args) throws Exception {
+
+        EmbeddedHelper.enableLogger(Level.OFF);
+        
+        DataSource ds = EmbeddedHelper.newDataSource(H2_JDBC_DRIVER, H2_JDBC_URL, H2_JDBC_USER, H2_JDBC_PASS);
+        RunScript.execute(ds.getConnection(), new InputStreamReader(TEIID3952Reproduce.class.getClassLoader().getResourceAsStream("teiid-3952/h2-schema.sql")));
+        
+//        execute(ds.getConnection(), "SELECT * FROM SampleTable", false);
+        
+        server = new EmbeddedServer();
+        
+        H2ExecutionFactory factory = new H2ExecutionFactory();
+        factory.start();
+        factory.setSupportsDirectQueryProcedure(true);
+        server.addTranslator("translator-h2", factory);
+        
+        server.addConnectionFactory("java:/accounts-ds", ds);
+        
+        EmbeddedConfiguration config = new EmbeddedConfiguration();
+        config.setTransactionManager(EmbeddedHelper.getTransactionManager());
+        server.start(config);
+                
+        server.deployVDB(TEIID3952Reproduce.class.getClassLoader().getResourceAsStream("teiid-3952/teiid3952-h2-vdb.xml"));
+        
+        Properties info = new Properties();
+        conn = server.getDriver().connect("jdbc:teiid:MatViewH2VDB", info);
+        
+        reproduce_1(ds.getConnection());
+        
+        conn.close();
+    }
+
+    /**
+     * 1) select a row from the materialized table
+     * 2) update a field in a row in the materialized view
+     * 3) select that row (value unchanged ie, same as int 1) - tried multiple times
+     * 
+     */
+    static void reproduce_1(Connection dsConn) throws Exception {
+        
+        execute(dsConn, "SELECT * FROM SampleTable WHERE id = '100'", false);
+
+        execute(conn, "SELECT * FROM SAMPLEMATVIEW WHERE id = '100'", false);
+        
+        execute(conn, "UPDATE SAMPLEMATVIEW SET a = 'aaa' WHERE id = '100'", false);
+        
+        for(int i = 0 ; i < 10 ; i++) {
+            
+            System.out.println("\n The " + (i + 1)  +" times query:\n");
+            
+            Thread.sleep(5000);
+            
+            execute(conn, "SELECT * FROM SAMPLEMATVIEW WHERE id = '100'", false);
+            
+            execute(dsConn, "SELECT * FROM SampleTable WHERE id = '100'", false);
+        }
+    }
+
+}
---
+++ perf/src/main/resources/teiid-3952/h2-schema.sql
@@ -0,0 +1,8 @@
+DROP TABLE IF EXISTS SampleTable;
+
+CREATE TABLE SampleTable(id CHAR(4), a CHAR(16), b CHAR(40), c CHAR(40));
+
+
+INSERT INTO SampleTable (id, a, b, c) VALUES ('100', 'a0', 'b0', 'c0');
+INSERT INTO SampleTable (id, a, b, c) VALUES ('101', 'a1', 'b1', 'c1');
+INSERT INTO SampleTable (id, a, b, c) VALUES ('102', 'a2', 'b2', 'c2');
