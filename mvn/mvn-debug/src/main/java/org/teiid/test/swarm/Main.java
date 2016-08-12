865a245725384d08c2bf3d492aacb34d6bb83cca
kylinsoong <kylinsoong.1214@gmail.com>
2016-08-12 21:36:35 +0800

    add teiid deployment test code

---
+++ mvn/mvn-debug/src/main/java/org/teiid/test/swarm/Main.java
@@ -0,0 +1,11 @@
+package org.teiid.test.swarm;
+
+public class Main {
+
+    public static void main(String[] args) throws Exception {
+        
+    }
+
+
+    
+}
--- teiid-deployment/src/main/java/org/teiid/test/swarm/Main.java
+++ teiid-deployment/src/main/java/org/teiid/test/swarm/Main.java
@@ -1,15 +1,34 @@
 package org.teiid.test.swarm;
 
+import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.wildfly.swarm.Swarm;
+import org.wildfly.swarm.container.Container;
+import org.wildfly.swarm.datasources.DatasourceArchive;
+import org.wildfly.swarm.teiid.VDBArchive;
 
 public class Main {
 
-    public static void main(String[] args) throws Exception {    	
+    public static void main(String[] args) throws Exception {
         
-        System.out.println("--------------");
+        // 1. start container, configure translator
+        Container container = new Container();
+        container.start();
         
-//        Container container = new Container();
-//        
-//        container.start();
+        // 2. deploy data sources/resource adapter
+        container.deploy(Swarm.artifact("com.h2database:h2", "h2"));
+        DatasourceArchive dsArchive = ShrinkWrap.create(DatasourceArchive.class);
+        dsArchive.dataSource("ExampleDS", (ds) -> {
+            ds.connectionUrl("jdbc:h2:mem:test-swarm;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+            ds.driverName("h2");
+            ds.userName("sa");
+            ds.password("sa");
+        });
+        container.deploy(dsArchive);
+        
+        // 3. deploy VDB
+        VDBArchive vdb = ShrinkWrap.create(VDBArchive.class);
+        vdb.vdb("portfolio-vdb.xml");
+        container.deploy(vdb);
         
     }
 
