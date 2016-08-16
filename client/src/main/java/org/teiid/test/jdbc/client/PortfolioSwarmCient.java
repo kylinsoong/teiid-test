2de7ff3c001267760d9feeef9a7e5616a8310ce3
kylinsoong <kylinsoong.1214@gmail.com>
2016-08-16 13:07:57 +0800

    https://issues.jboss.org/browse/SWARM-602

---
+++ client/src/main/java/org/teiid/test/jdbc/client/PortfolioSwarmCient.java
@@ -0,0 +1,25 @@
+package org.teiid.test.jdbc.client;
+
+import static org.teiid.test.jdbc.client.JDBCUtils.execute;
+import static org.teiid.test.jdbc.client.JDBCUtils.getDriverConnection;
+
+import java.sql.Connection;
+
+public class PortfolioSwarmCient {
+    
+    private static final String JDBC_DRIVER = "org.teiid.jdbc.TeiidDriver";
+    private static final String JDBC_URL = "jdbc:teiid:Portfolio@mm://localhost:31000;version=1";
+    private static final String JDBC_USER = "teiidUser";
+    private static final String JDBC_PASS = "password1!";
+    
+    static String SQL_QUERY_1 = "SELECT * FROM product";
+
+    public static void main(String[] args) throws Exception {
+        
+        Connection conn = getDriverConnection(JDBC_DRIVER, JDBC_URL, JDBC_USER, JDBC_PASS);
+        
+        execute(conn, SQL_QUERY_1, false);
+
+    }
+
+}
--- teiid-deployment/src/main/java/org/teiid/test/swarm/Main.java
+++ teiid-deployment/src/main/java/org/teiid/test/swarm/Main.java
@@ -1,35 +1,56 @@
 package org.teiid.test.swarm;
 
 import org.jboss.shrinkwrap.api.ShrinkWrap;
+import org.jboss.shrinkwrap.api.asset.ClassLoaderAsset;
 import org.wildfly.swarm.Swarm;
-import org.wildfly.swarm.container.Container;
 import org.wildfly.swarm.datasources.DatasourceArchive;
+import org.wildfly.swarm.resource.adapters.RARArchive;
 import org.wildfly.swarm.teiid.VDBArchive;
+//import org.wildfly.swarm.datasources.DatasourceArchive;
+//import org.wildfly.swarm.resource.adapters.RARArchive;
+//import org.wildfly.swarm.teiid.VDBArchive;
 
 public class Main {
 
     public static void main(String[] args) throws Exception {
         
         // 1. start container, configure translator
-        Container container = new Container();
-        container.start();
+        Swarm swarm = new Swarm();
+        swarm.start();
+        
+//        Container container = new Container();
+//        container.start();
         
         // 2. deploy data sources/resource adapter
-        container.deploy(Swarm.artifact("com.h2database:h2", "h2"));
+        swarm.deploy(Swarm.artifact("com.h2database:h2", "h2"));
         DatasourceArchive dsArchive = ShrinkWrap.create(DatasourceArchive.class);
-        dsArchive.dataSource("ExampleDS", (ds) -> {
-            ds.connectionUrl("jdbc:h2:mem:test-swarm;DB_CLOSE_DELAY=-1;DB_CLOSE_ON_EXIT=FALSE");
+        dsArchive.dataSource("accounts-ds", (ds) -> {
+            ds.connectionUrl("jdbc:h2:mem:accounts;INIT=RUNSCRIPT FROM '/home/kylin/server/teiid-9.0.0.Final/teiidfiles/customer-schema.sql';");
             ds.driverName("h2");
             ds.userName("sa");
             ds.password("sa");
         });
-        container.deploy(dsArchive);
+        swarm.deploy(dsArchive);
         
+        RARArchive rarArchive = ShrinkWrap.create(RARArchive.class);
+//        rarArchive.resourceAdapter(new ClassLoaderAsset("file-connector.xml", Main.class.getClassLoader()));
+        rarArchive.resourceAdapter("fileQS", ra -> {
+            ra.module("org.jboss.teiid.resource-adapter.file");
+            ra.connectionDefinitions("fileConnections", d -> {
+                d.className("org.teiid.resource.adapter.file.FileManagedConnectionFactory");
+                d.jndiName("java:/marketdata-file");
+                d.enabled(true);
+                d.useJavaContext(true);
+                d.configProperties("AllowParentPaths", p -> p.value("true"));
+                d.configProperties("ParentDirectory", p -> p.value("/home/kylin/server/teiid-9.0.0.Final/teiidfiles/data"));
+            });
+        });
+        swarm.deploy(rarArchive);
+
         // 3. deploy VDB
         VDBArchive vdb = ShrinkWrap.create(VDBArchive.class);
-        vdb.vdb("portfolio-vdb.xml");
-        container.deploy(vdb);
-        
+        vdb.vdb(Main.class.getClassLoader().getResourceAsStream("portfolio-vdb.xml"));
+        swarm.deploy(vdb);     
     }
 
 
